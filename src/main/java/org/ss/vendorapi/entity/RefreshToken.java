167f9601724ccaf72aa7aab69a50c808d3cd1ffe
Amit Rawat <rawat.amit158@gmail.com>
2024-08-02 18:12:24 +0530

    add

--- src/main/java/org/ss/vendorapi/entity/RefreshToken.java
+++ src/main/java/org/ss/vendorapi/entity/RefreshToken.java
@@ -1,26 +1,4 @@
 package org.ss.vendorapi.entity;
-
-<<<<<<< HEAD
-import java.time.Instant;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "REFRESHTOKEN")
-public class RefreshToken {
-	
-	  @Id
-	  @Column(name = "id")
-=======
 import java.io.Serializable;
 import java.time.Instant;
 
@@ -45,7 +23,7 @@ public class RefreshToken implements Serializable{
 
 	@Id
 	  @GeneratedValue(strategy = GenerationType.IDENTITY)
->>>>>>> branch 'main' of https://github.com/gusainanukriti/CVMS_SERVICES.git
+
 	  private int id;
 
 	  @Column(name = "user_id")
