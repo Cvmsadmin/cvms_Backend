132b5729b7d0641f16c790aa41f8485bf68cdaeb
Amit Rawat <rawat.amit158@gmail.com>
2024-08-06 17:10:37 +0530

    change in Forgot password

---
+++ src/main/java/org/ss/vendorapi/modal/ForgotPasswordRequest.java
@@ -0,0 +1,48 @@
+package org.ss.vendorapi.modal;
+
+
+
+public class ForgotPasswordRequest {
+	
+	
+	private String email;
+    private String phone;
+    private String newPassword;
+    private String confirmPassword;
+
+    // Getters and Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+	
+	
+
+}
